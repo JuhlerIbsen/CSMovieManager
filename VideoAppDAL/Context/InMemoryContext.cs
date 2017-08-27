79b748fc77e6b9b98993aae4d43132787ab1ac63
Michael Ibsen <michael_c_ibsen@hotmail.com>
2017-08-27 20:46:44 +0200

    Added entityframework and made it functional
    Implemented entity framework.

---
+++ VideoAppDAL/Context/InMemoryContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSVideoMenu;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace VideoAppDAL.Context
+{
+    class InMemoryContext : DbContext
+    {
+        private static DbContextOptions<InMemoryContext> options =
+            new DbContextOptionsBuilder<InMemoryContext>().UseInMemoryDatabase("VideoDB").Options;
+
+        public InMemoryContext() : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ VideoAppDAL/Repositorys/MovieRepositoryEFMemory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSVideoMenu;
+using VideoAppDAL.Context;
+
+namespace VideoAppDAL.Repositorys
+{
+    class MovieRepositoryEFMemorycs : IRepository<Movie>
+    {
+        private InMemoryContext context;
+
+        public MovieRepositoryEFMemorycs(InMemoryContext context)
+        {
+            this.context = context;
+        }
+
+
+        public Movie Add(Movie movie)
+        {
+            context.Movies.Add(movie);
+            context.SaveChanges();
+            return movie;
+        }
+
+        public List<Movie> ListAll()
+        {
+            return context.Movies.ToList();
+        }
+
+        public Movie FindById(int movieId)
+        {
+            return context.Movies.FirstOrDefault(movie => movie.Id == movieId);
+        }
+
+        public bool Delete(int movieId)
+        {
+            var movie = FindById(movieId);
+            context.Movies.Remove(movie);
+            context.SaveChanges();
+            return movie != null;
+        }
+    }
+}
