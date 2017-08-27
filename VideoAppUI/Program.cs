a3cdcdb0e812a1a1054b9b7698882d62268fe2d8
Michael Ibsen <michael_c_ibsen@hotmail.com>
2017-08-27 19:14:35 +0200

    OldApplication renamed to MovieApplication and moved to its respective location in the 3-layer-architecture.
    That should be it for this assignment, next one is coming up.

--- VideoAppUI/Program.cs
+++ VideoAppUI/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using CSVideoMenu;
+using VideoAppBLL;
 
 namespace VideoAppUI
 {
     class Program
     {
+       
+       static BLLFacade bllFacade = new BLLFacade();
+
+        /// <summary>
+        /// All magic starts here.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            OldApplication.RunApplication();
+           
+          // Run console application.
+          MovieApplication.RunApplication();
         }
+
     }
 }
