8c640f0997f2aa3517b59cd5c9786efbf1fce07b
Michael Ibsen <michael_c_ibsen@hotmail.com>
2017-08-27 17:36:28 +0200

    Made the 3 layer architecture and moved the project.
    There has been done no changes to any of the code yet, it has simply been moved.

---
+++ VideoAppEntity/Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSVideoMenu
+{
+    public class Movie
+    {
+        
+        // Enums should probably not be in the same class as the entity.
+        // Did it anyway.. (Mwhaha)
+        public enum Genre
+        {
+            NoGenre,
+            Comedy,
+            Horror,
+            Romantique
+        }
+
+        public enum FileType
+        {
+            MP4,
+            MKV,
+            AVI
+        }
+
+        private static int _id = 0;
+
+        public Movie()
+        {
+            _id ++;
+            Id = _id;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public FileType Extention { get; set; }
+        public Genre MovieGenre { get; set; }
+        public long Duration { get; set; }
+
+    }
+}
---
+++ VideoAppUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CSVideoMenu;
+
+namespace VideoAppUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OldApplication.RunApplication();
+        }
+    }
+}
