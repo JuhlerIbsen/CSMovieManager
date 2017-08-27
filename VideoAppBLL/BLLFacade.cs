a7bc2e430b3f5964a0aa38972bc7541c0a84020e
Michael Ibsen <michael_c_ibsen@hotmail.com>
2017-08-27 18:50:19 +0200

    Populated the facade
    This is called the facade patern, so we can access everything from the facade.

--- VideoAppBLL/BLLFacade.cs
+++ VideoAppBLL/BLLFacade.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CSVideoMenu;
+using VideoAppBLL.Services;
 
 namespace VideoAppBLL
 {
     public class BLLFacade
     {
+
+        public IService<Movie> VideoService => (new VideoService());
+
+        /// <summary>
+        /// Return VideoService which can use c.r.u.d operations.
+        /// </summary>
+        /// <returns>VideoService</returns>
+        public IService<Movie> GetVideoService()
+        {
+            return (new VideoService());
+        }
+
     }
 }
